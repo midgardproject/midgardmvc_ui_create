3bf391b8d101981e3e0b4479718785b9170ef7a3
Henri Bergius <henri.bergius@iki.fi>
2010-12-09 02:24:30 +0200

    Load Aloha from a proper Git submodule

--- static/aloha.js
+++ static/aloha.js
@@ -1,8 +1,8 @@
 // Inform Aloha of our installation directory
-GENTICS_Aloha_base = '/midgardmvc-static/midgardmvc_ui_create/aloha/';
+GENTICS_Aloha_base = '/midgardmvc-static/midgardmvc_ui_create/Aloha-Editor/WebContent/';
 
 // Include main Aloha
-document.write('<script type="text/javascript" src="' + GENTICS_Aloha_base + 'aloha.js"></script>');
+document.write('<script type="text/javascript" src="' + GENTICS_Aloha_base + 'core/include.js"></script>');
 document.write('<script type="text/javascript" src="' + GENTICS_Aloha_base + 'plugins/com.gentics.aloha.plugins.HighlightEditables/plugin.js"></script>');
 document.write('<script type="text/javascript" src="' +GENTICS_Aloha_base + 'plugins/com.gentics.aloha.plugins.Format/plugin.js"></script>');
 document.write('<script type="text/javascript" src="' +GENTICS_Aloha_base + 'plugins/com.gentics.aloha.plugins.List/plugin.js"></script>');
