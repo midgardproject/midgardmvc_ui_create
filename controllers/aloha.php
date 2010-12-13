61b444c120cbb6585cd547c37234230516b2ed94
Henri Bergius <henri.bergius@iki.fi>
2010-12-13 15:54:35 +0200

    Map RDF properties

--- controllers/aloha.php
+++ controllers/aloha.php
@@ -19,14 +19,22 @@ class midgardmvc_ui_create_controllers_aloha
 
     public function post_save(array $args)
     {
-        if (isset($args['guid']))
+        if (!isset($_POST['type']))
         {
-            $this->load_object($args);
+            throw new midgardmvc_exception_notfound("No type provided");
+        }
+        $mgdschema = $this->get_class($_POST['type']);
+        unset($_POST['type']);
+
+        if (isset($_POST['identifier']))
+        {
+            $this->load_object($mgdschema, $_POST['identifier']);
+            unset($_POST['identifier']);
             midgardmvc_core::get_instance()->authorization->require_do('midgard:update', $this->object);
         }
         else
         {
-            $this->prepare_new_object($args);
+            $this->prepare_new_object($mgdschema);
             if (isset($this->parent))
             {
                 midgardmvc_core::get_instance()->authorization->require_do('midgard:create', $this->parent);
@@ -39,10 +47,10 @@ class midgardmvc_ui_create_controllers_aloha
         }
 
         // Process with form
-        $this->form = midgardmvc_helper_forms::create($args['type']);
+        $this->form = midgardmvc_helper_forms::create($mgdschema);
         try
         {
-            $this->process_form();
+            $this->process_form($mgdschema);
             if ($this->object->guid)
             {
                 $this->object->update();
@@ -68,53 +76,165 @@ class midgardmvc_ui_create_controllers_aloha
         }
     }
 
-    public function process_form()
+    public function process_form($mgdschema)
     {
+        $property_map = array();
         foreach ($_POST as $property => $value)
         {
-            if (!property_exists($this->object, $property))
-            {
-                throw new InvalidArgumentException(get_class($this->object) . " objects don't have property {$property}");
-            }
-
-            midgardmvc_helper_forms_mgdschema::property_to_form(get_class($this->object), $property, $this->object->$property, $this->form);
+            $property_map[$property] = $this->map_property($mgdschema, $property);
+            midgardmvc_helper_forms_mgdschema::property_to_form($mgdschema, $mgd_property, $this->object->$mgd_property, $this->form, $property);
         }
         $this->form->process_post();
         foreach ($_POST as $property => $value)
         {
-            $this->object->$property = $this->form->$property->get_value();
+            $mgd_property = $property_map[$property];
+            $this->object->$mgd_property = $this->form->items[$property]->get_value();
         }
     }
 
-    public function load_object(array $args)
+    private function clean_namespace($identifier)
     {
-        $class = $args['type'];
-        if (!class_exists($class))
+        if (substr($identifier, 0, 4) == 'mgd:')
         {
-            throw new midgardmvc_exception_notfound("{$class} is not installed");
+            return substr($identifier, 4);
         }
+        if (substr($identifier, 0, 9) == 'urn:uuid:')
+        {
+            return substr($identifier, 9);
+        }
+        return $identifier;
+    }
+
+    private function get_namespace_map($type)
+    {
+        static $nsmap = array();
+        if (isset($nsmap[$type]))
+        {
+            return $nsmap[$type];
+        }
+
         $mgdschemas = midgardmvc_core::get_instance()->dispatcher->get_mgdschema_classes();
-        if (!in_array($class, $mgdschemas))
+        if (!in_array($type, $mgdschemas))
         {
-            throw new midgardmvc_exception_notfound("{$class} is not a Midgard type");
+            throw new midgardmvc_exception_notfound("Type {$type} is not a registered MgdSchema");
         }
-        $this->object = new $class($args['guid']);
+
+        $nsmap[$type] = array
+        (
+            'mgd' => 'http://www.midgard-project.org/midgard2/9.03',
+        );
+
+        $reflector = new midgard_reflection_class($type);
+        $namespaces = $reflector->get_user_value('namespaces');
+        if (empty($namespaces))
+        {
+            // Type didn't register additional namespaces
+            return $nsmap[$type];
+        }
+
+        $namespaces = explode(',', $namespaces);
+        foreach ($namespaces as $namespace)
+        {
+            $namespace_parts = explode(':', $namespace, 2);
+            $nsmap[$type][$namespace_parts[0]] = $namespace_parts[1];
+        }
+
+        return $nsmap[$type];
+    }
+
+    private function map_property($mgdschema, $property)
+    {
+        static $property_map = array();
+        if (!isset($property_map[$mgdschema]))
+        {
+            $property_map[$mgdschema] = array();
+        }
+
+        $nsmap = $this->get_namespace_map($mgdschema);
+        $reflector = new midgard_reflection_class($mgdschema);
+        $props = $reflector->getProperties();
+        foreach ($props as $prop)
+        {
+            if ($property == $prop->name)
+            {
+                // Straight match
+                $property_map[$mgdschema][$property] = $prop->name;
+                break 1;
+            }
+
+            $property_reflector = new midgard_reflection_property($mgdschema);
+            $namespaced_property = $property_reflector->get_user_value($prop->name, 'property');
+            if (empty($namespaced_property))
+            {
+                // No namespaced property mappings
+                continue;
+            }
+
+            foreach ($nsmap as $prefix => $url)
+            {
+                if (substr($property, 0, strlen($url)) == $url)
+                {
+                    $property_map[$mgdschema][$property] = $prop->name;
+                    break 2;
+                }
+                echo substr($property, 0, strlen($url));
+
+                $prefix_colon = "{$prefix}:";
+                if (substr($property, 0, strlen($prefix_colon)) == $prefix_colon)
+                {
+                    $property_map[$mgdschema][$property] = $prop->name;
+                    break 2;
+                }
+                die(substr($property, 0, strlen($prefix_colon)));
+            }
+        }
+
+        if (!isset($property_map[$mgdschema][$property]))
+        {
+            var_dump($property_map);
+            die();
+            throw new midgardmvc_exception_notfound("Unable to map {$property} to a property of {$mgdschema}");
+        }
+
+        return $property_map[$mgdschema][$property];
+    }
+
+    private function get_class($type)
+    {
+        $mgdschemas = midgardmvc_core::get_instance()->dispatcher->get_mgdschema_classes();
+        if (substr($type, 0, 4) == 'mgd:')
+        {
+            // Straight reference to a MgdSchema type
+            $class = $this->clean_namespace($type);
+            if (!in_array($class, $mgdschemas))
+            {
+                throw new midgardmvc_exception_notfound("Class {$class} is not a MgdSchema type");
+            }
+            return $class;
+        }
+
+        // Check for RDF type match
+        foreach ($mgdschemas as $mgdschema)
+        {
+            $reflector = new midgard_reflection_class($mgdschema);
+            if ($reflector->get_user_value('typeof') == $type)
+            {
+                return $mgdschema;
+            }
+        }
+
+        throw new midgardmvc_exception_notfound("Unrecognized MgdSchema type {$class}");
+    }
+
+    public function load_object($mgdschema, $guid)
+    {
+        $guid = $this->clean_namespace($guid);
+        $this->object = new $mgdschema($guid);
     }
     
-    public function prepare_new_object(array $args)
+    public function prepare_new_object($mgdschema)
     {
-        $class = $args['type'];
-        if (!class_exists($class))
-        {
-            throw new midgardmvc_exception_notfound("{$class} is not installed");
-        }
-        $instance = new $class();
-        if (!$instance instanceof midgard_object)
-        {
-            throw new midgardmvc_exception_notfound("{$class} is not a Midgard type");
-        }
-        unset($instance);
-        $this->object = new $class();
+        $this->object = new $mgdschema();
     }
 }
 ?>
--- static/aloha.js
+++ static/aloha.js
@@ -3,7 +3,6 @@ GENTICS_Aloha_base = '/midgardmvc-static/midgardmvc_ui_create/Aloha-Editor/WebCo
 
 // Include main Aloha
 document.write('<script type="text/javascript" src="' + GENTICS_Aloha_base + 'core/include.js"></script>');
-document.write('<script type="text/javascript" src="' + GENTICS_Aloha_base + 'plugins/com.gentics.aloha.plugins.HighlightEditables/plugin.js"></script>');
 document.write('<script type="text/javascript" src="' +GENTICS_Aloha_base + 'plugins/com.gentics.aloha.plugins.Format/plugin.js"></script>');
 document.write('<script type="text/javascript" src="' +GENTICS_Aloha_base + 'plugins/com.gentics.aloha.plugins.List/plugin.js"></script>');
 document.write('<script type="text/javascript" src="' +GENTICS_Aloha_base + 'plugins/org.midgardproject.aloha.plugins.Save/plugin.js"></script>');
@@ -12,37 +11,7 @@ document.write('<script type="text/javascript" src="' +GENTICS_Aloha_base + 'plu
 jQuery(document).ready(function() {
     GENTICS.Aloha.settings = {
         "ribbon": true,
-        "language": "en",
+        "language": "en"
     };
-
-    var objectcontainers = jQuery('[mgd\\:guid]');
-    jQuery.each(objectcontainers, function(index, objectinstance)
-    {
-        var objectinstance = jQuery(objectinstance);
-        var children = jQuery('*', objectinstance).filter(function() {
-            return jQuery(this).attr('mgd:property'); 
-        });
-        var guid = objectinstance.attr('mgd:guid');
-        if (!guid)
-        {
-            return true;
-        }
-        var type = objectinstance.attr('mgd:type');
-        
-        if (typeof midgardproject.SavePlugin.objects[guid] == "undefined") {
-            midgardproject.SavePlugin.objects[guid] = {};
-        }
-
-        midgardproject.SavePlugin.objects[guid].type = type;
-        midgardproject.SavePlugin.objects[guid].element = objectinstance;
-        midgardproject.SavePlugin.objects[guid].properties = {};
-
-        jQuery.each(children, function(index, childinstance)
-        {
-            var childinstance = jQuery(childinstance);
-            var propertyName = childinstance.attr('mgd:property');
-            midgardproject.SavePlugin.objects[guid].properties[propertyName] = new GENTICS.Aloha.Editable(childinstance);
-            childinstance.MIDGARDMVC_UI_CREATE_GUID = guid;
-        });
-    });
 });
+
