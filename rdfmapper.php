fc0ec0e0f81c9a8e7830e174e12ad9a498de2481
Henri Bergius <henri.bergius@iki.fi>
2010-12-14 13:48:08 +0200

    New RDF mapping helper

---
+++ rdfmapper.php
@@ -0,0 +1,151 @@
+<?php
+class midgardmvc_ui_create_rdfmapper
+{
+    private static function get_mgdschemas()
+    {
+        // FIXME: pending on https://github.com/midgardproject/midgard-php5/issues/issue/17
+        return array('midgard_person', 'org_midgardproject_news_article');
+        return midgardmvc_core::get_instance()->dispatcher->get_mgdschema_classes();
+    }
+
+    private static function expand_prefix($value, array $namespaces)
+    {
+        foreach ($namespaces as $prefix => $url)
+        {
+            if (substr($value, 0, strlen($prefix)) == $prefix)
+            {
+                // Expand to prefix to URL
+                return str_replace("{$prefix}:", $url, $value);
+            }
+
+            if (substr($value, 0, strlen($url)) == $url)
+            {
+                // Full URL match
+                return $value;
+            }
+        }
+        return "{$namespaces['mgd']}{$value}";
+    }
+
+    public static function get_namespace_map($type)
+    {
+        static $nsmap = array();
+        if (isset($nsmap[$type]))
+        {
+            return $nsmap[$type];
+        }
+
+        $mgdschemas = self::get_mgdschemas();
+        if (!in_array($type, $mgdschemas))
+        {
+            throw new midgardmvc_exception_notfound("Type {$type} is not a registered MgdSchema");
+        }
+
+        $nsmap[$type] = array
+        (
+            'mgd' => 'http://www.midgard-project.org/midgard2/9.03/',
+        );
+
+        $reflector = new midgard_reflection_class($type);
+        $namespaces = $reflector->get_user_value('namespaces');
+        if (empty($namespaces))
+        {
+            // Type didn't register additional namespaces
+            return $nsmap[$type];
+        }
+
+        $namespaces = explode(',', $namespaces);
+        foreach ($namespaces as $namespace)
+        {
+            $namespace_parts = explode(':', $namespace, 2);
+            $nsmap[$type][$namespace_parts[0]] = $namespace_parts[1];
+        }
+
+        return $nsmap[$type];
+    }
+
+    public static function map_class($type)
+    {
+        static $mapped_classes = array();
+        if (isset($mapped_classes[$type]))
+        {
+            return $mapped_classes[$type];
+        }
+
+        // Load class mappings
+        $mgdschemas = self::get_mgdschemas();
+        // Check for RDF type match
+        foreach ($mgdschemas as $mgdschema)
+        {
+            $namespaces = self::get_namespace_map($mgdschema);
+            $reflector = new midgard_reflection_class($mgdschema);
+            $typeof = $reflector->get_user_value('typeof');
+            if (!$typeof)
+            {
+                $typeof = "{$namespaces['mgd']}{$mgdschema}";
+            }
+
+            if ($typeof == $type)
+            {
+                // Straight, fully-qualified match
+                $mapped_classes[$type] = $mgdschema;
+                return $mapped_classes[$type];
+            }
+
+            foreach ($namespaces as $prefix => $url)
+            {
+                $nstype = $type;
+                if (substr($type, 0, strlen($prefix)) == $prefix)
+                {
+                    $nstype = str_replace("{$prefix}:", $url, $type);
+                }
+
+                if ($typeof == $nstype)
+                {
+                    $mapped_classes[$type] = $mgdschema;
+                    return $mapped_classes[$type];
+                }
+            }
+        }
+
+        throw new midgardmvc_exception_notfound("Unrecognized MgdSchema type {$type}");
+    }
+
+    public static function map_property($mgdschema, $property)
+    {
+        static $property_map = array();
+        if (!isset($property_map[$mgdschema]))
+        {
+            $property_map[$mgdschema] = array();
+        }
+
+        if (isset($property_map[$mgdschema][$property]))
+        {
+            return $property_map[$mgdschema][$property];
+        }
+
+        $namespaces = self::get_namespace_map($mgdschema);
+        $namespaced_property = self::expand_prefix($property, $namespaces);
+        
+        $dummy = new $mgdschema();
+        $props = get_object_vars($dummy);
+        $reflector = new midgard_reflection_property($mgdschema);
+
+        foreach ($props as $prop => $value)
+        {
+            $rdfprop = $reflector->get_user_value($prop, 'property');
+            if (!$rdfprop)
+            {
+                $rdfprop = $prop;
+            }
+            $nsprop = self::expand_prefix($rdfprop, $namespaces);
+            if ($namespaced_property == $nsprop)
+            {
+                $property_map[$mgdschema][$property] = $prop;
+                return $property_map[$mgdschema][$property];
+            }
+        }
+
+        throw new midgardmvc_exception_notfound("Unable to map {$property} to a property of {$mgdschema}");
+    }
+}
---
+++ tests/rdfmapper.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package midgardmvc_core
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * Unit tests for the RDF mapper class
+ *
+ * @package midgardmvc_core
+ */
+class midgardmvc_ui_create_tests_rdfmapper extends midgardmvc_core_tests_testcase
+{
+    public function test_map_found()
+    {
+        $person_class = midgardmvc_ui_create_rdfmapper::map_class('http://xmlns.com/foaf/0.1/Person');
+        $this->assertEquals('midgard_person', $person_class);
+
+        $person_class = midgardmvc_ui_create_rdfmapper::map_class('foaf:Person');
+        $this->assertEquals('midgard_person', $person_class);
+
+        //$person_class = midgardmvc_ui_create_rdfmapper::map_class('mgd:midgard_snippet');
+        //$this->assertEquals('midgard_snippet', $person_class);
+    }
+
+    /**
+     * @depends test_map_found
+     * @expectedException midgardmvc_exception_notfound
+     */
+    public function test_map_found_invalid()
+    {
+        // Person has a proper RDF mapping so this should not work
+        $person_class = midgardmvc_ui_create_rdfmapper::map_class('mgd:person');
+        $this->assertEquals('midgard_person', $person_class);
+    }
+
+    /**
+     * @expectedException midgardmvc_exception_notfound
+     */
+    public function test_map_not_found()
+    {
+        $person_class = midgardmvc_ui_create_rdfmapper::map_class('http://example.net/foo');
+    }
+
+    /**
+     * @depends test_map_found
+     */
+    public function test_property_found()
+    {
+        $person_class = midgardmvc_ui_create_rdfmapper::map_class('http://xmlns.com/foaf/0.1/Person');
+
+        $property = midgardmvc_ui_create_rdfmapper::map_property($person_class, 'foaf:firstName');
+        $this->assertEquals('firstname', $property);
+
+        $property = midgardmvc_ui_create_rdfmapper::map_property($person_class, 'foaf:lastName');
+        $this->assertEquals('lastname', $property);
+
+        $property = midgardmvc_ui_create_rdfmapper::map_property($person_class, 'mgd:id');
+        $this->assertEquals('id', $property);
+    }
+
+    /**
+     * @depends test_property_found
+     * @expectedException midgardmvc_exception_notfound
+     */
+    public function test_property_found_invalid()
+    {
+        // Person firstname has a proper RDF mapping so this should not work
+        $person_class = midgardmvc_ui_create_rdfmapper::map_class('http://xmlns.com/foaf/0.1/Person');
+
+        $property = midgardmvc_ui_create_rdfmapper::map_property($person_class, 'mgd:firstname');
+    }
+}
+?>
