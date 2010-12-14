90e5f7cf53bbd250a83f3b4f35313e63b6ac5c08
Henri Bergius <henri.bergius@iki.fi>
2010-12-14 13:48:21 +0200

    Use the new RDF mapping helper

--- controllers/aloha.php
+++ controllers/aloha.php
@@ -23,7 +23,7 @@ class midgardmvc_ui_create_controllers_aloha
         {
             throw new midgardmvc_exception_notfound("No type provided");
         }
-        $mgdschema = $this->get_class($_POST['type']);
+        $mgdschema = midgardmvc_ui_create_rdfmapper::map_class($_POST['type']);
         unset($_POST['type']);
 
         if (isset($_POST['identifier']))
@@ -78,16 +78,15 @@ class midgardmvc_ui_create_controllers_aloha
 
     public function process_form($mgdschema)
     {
-        $property_map = array();
         foreach ($_POST as $property => $value)
         {
-            $property_map[$property] = $this->map_property($mgdschema, $property);
+            $mgd_property = midgardmvc_ui_create_rdfmapper::map_property($mgdschema, $property);
             midgardmvc_helper_forms_mgdschema::property_to_form($mgdschema, $mgd_property, $this->object->$mgd_property, $this->form, $property);
         }
         $this->form->process_post();
         foreach ($_POST as $property => $value)
         {
-            $mgd_property = $property_map[$property];
+            $mgd_property = midgardmvc_ui_create_rdfmapper::map_property($mgdschema, $property);
             $this->object->$mgd_property = $this->form->items[$property]->get_value();
         }
     }
@@ -103,127 +102,6 @@ class midgardmvc_ui_create_controllers_aloha
             return substr($identifier, 9);
         }
         return $identifier;
-    }
-
-    private function get_namespace_map($type)
-    {
-        static $nsmap = array();
-        if (isset($nsmap[$type]))
-        {
-            return $nsmap[$type];
-        }
-
-        $mgdschemas = midgardmvc_core::get_instance()->dispatcher->get_mgdschema_classes();
-        if (!in_array($type, $mgdschemas))
-        {
-            throw new midgardmvc_exception_notfound("Type {$type} is not a registered MgdSchema");
-        }
-
-        $nsmap[$type] = array
-        (
-            'mgd' => 'http://www.midgard-project.org/midgard2/9.03',
-        );
-
-        $reflector = new midgard_reflection_class($type);
-        $namespaces = $reflector->get_user_value('namespaces');
-        if (empty($namespaces))
-        {
-            // Type didn't register additional namespaces
-            return $nsmap[$type];
-        }
-
-        $namespaces = explode(',', $namespaces);
-        foreach ($namespaces as $namespace)
-        {
-            $namespace_parts = explode(':', $namespace, 2);
-            $nsmap[$type][$namespace_parts[0]] = $namespace_parts[1];
-        }
-
-        return $nsmap[$type];
-    }
-
-    private function map_property($mgdschema, $property)
-    {
-        static $property_map = array();
-        if (!isset($property_map[$mgdschema]))
-        {
-            $property_map[$mgdschema] = array();
-        }
-
-        $nsmap = $this->get_namespace_map($mgdschema);
-        $reflector = new midgard_reflection_class($mgdschema);
-        $props = $reflector->getProperties();
-        foreach ($props as $prop)
-        {
-            if ($property == $prop->name)
-            {
-                // Straight match
-                $property_map[$mgdschema][$property] = $prop->name;
-                break 1;
-            }
-
-            $property_reflector = new midgard_reflection_property($mgdschema);
-            $namespaced_property = $property_reflector->get_user_value($prop->name, 'property');
-            if (empty($namespaced_property))
-            {
-                // No namespaced property mappings
-                continue;
-            }
-
-            foreach ($nsmap as $prefix => $url)
-            {
-                if (substr($property, 0, strlen($url)) == $url)
-                {
-                    $property_map[$mgdschema][$property] = $prop->name;
-                    break 2;
-                }
-                echo substr($property, 0, strlen($url));
-
-                $prefix_colon = "{$prefix}:";
-                if (substr($property, 0, strlen($prefix_colon)) == $prefix_colon)
-                {
-                    $property_map[$mgdschema][$property] = $prop->name;
-                    break 2;
-                }
-                die(substr($property, 0, strlen($prefix_colon)));
-            }
-        }
-
-        if (!isset($property_map[$mgdschema][$property]))
-        {
-            var_dump($property_map);
-            die();
-            throw new midgardmvc_exception_notfound("Unable to map {$property} to a property of {$mgdschema}");
-        }
-
-        return $property_map[$mgdschema][$property];
-    }
-
-    private function get_class($type)
-    {
-        $mgdschemas = midgardmvc_core::get_instance()->dispatcher->get_mgdschema_classes();
-        if (substr($type, 0, 4) == 'mgd:')
-        {
-            // Straight reference to a MgdSchema type
-            $class = $this->clean_namespace($type);
-            if (!in_array($class, $mgdschemas))
-            {
-                throw new midgardmvc_exception_notfound("Class {$class} is not a MgdSchema type");
-            }
-            return $class;
-        }
-
-        // Check for RDF type match
-        foreach ($mgdschemas as $mgdschema)
-        {
-            $reflector = new midgard_reflection_class($mgdschema);
-            if ($reflector->get_user_value('typeof') == $type)
-            {
-                return $mgdschema;
-            }
-        }
-
-        throw new midgardmvc_exception_notfound("Unrecognized MgdSchema type {$class}");
     }
 
     public function load_object($mgdschema, $guid)
