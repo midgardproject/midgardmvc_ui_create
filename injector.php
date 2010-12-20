d670ff7b87bcce48cf57a8405991cb65e4a7324a
Henri Bergius <henri.bergius@iki.fi>
2010-12-20 22:21:07 +0200

    Centralize CMS access checks

--- injector.php
+++ injector.php
@@ -13,22 +13,39 @@
  */
 class midgardmvc_ui_create_injector
 {
+    /**
+     * Check whether a user is permitted to use the CMS
+     */
+    public static function can_use()
+    {
+        if (!midgardmvc_core::get_instance()->authentication->is_user())
+        {
+            return false;
+        }
+
+        if (midgardmvc_core::get_instance()->authentication->get_user()->usertype != MIDGARD_USER_TYPE_ADMIN)
+        {
+            // We distinquish between CMS users and end-users by ADMIN vs. USER level
+            return false;
+        }
+
+        return true;
+    }
+
     public function inject_process(midgardmvc_core_request $request)
     {
         // Register URL handlers
         $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('midgardmvc_ui_create'));
-
-        if (!midgardmvc_core::get_instance()->authentication->is_user())
-        {
-            return;
-        }
 
         if (midgardmvc_core::get_instance()->context->get_current_context() != 0)
         {
             return;
         }
 
-        // TODO: Check that user is permitted to use the CMS UI
+        if (!self::can_use())
+        {
+            return;
+        }
 
         midgardmvc_core::get_instance()->head->enable_jquery();
         midgardmvc_core::get_instance()->head->add_jsfile(MIDGARDMVC_STATIC_URL . '/midgardmvc_core/jQuery/jquery-ui-1.8.7.min.js');
